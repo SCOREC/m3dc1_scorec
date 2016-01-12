64b92c73fb2b6fe65c603b6d5f29042120f5c9a8
Kaushik Kalyanaraman <dialkforkaushik@gmail.com>
2016-01-12 18:44:38 -0500

    Added path to omega_h

--- scorec_build/setup.avatar.sh
+++ scorec_build/setup.avatar.sh
@@ -2,6 +2,7 @@ module load mpich3/latest
 module load cmake/latest
 
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/lore/kalyak/avatar/m3dc1/core-install
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/lore/kalyak/avatar/m3dc1/interface-install
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/lore/kalyak/avatar/m3dc1/omega_h-install
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/lore/kalyak/avatar/petsc-install
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/lore/kalyak/avatar/zoltan-install
