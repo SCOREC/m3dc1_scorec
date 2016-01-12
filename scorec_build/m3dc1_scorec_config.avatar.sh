ec16f471ed3b7832d2ccc84657f5dfdf5049c893
Kaushik Kalyanaraman <dialkforkaushik@gmail.com>
2016-01-12 18:45:14 -0500

    Turned off complex configuratiion switch

--- scorec_build/m3dc1_scorec_config.avatar.sh
+++ scorec_build/m3dc1_scorec_config.avatar.sh
@@ -7,7 +7,7 @@ cmake .. \
   -DCMAKE_C_FLAGS="-g -O2" \
   -DCMAKE_CXX_FLAGS="-g -O2" \
   -DCMAKE_Fortran_FLAGS="-O2 -g -c -fdefault-real-8 -Wall -cpp"\
-  -DENABLE_COMPLEX=ON \
+  -DENABLE_COMPLEX=OFF \
   -DENABLE_MESHGEN=OFF \
   -DENABLE_TESTING=ON \
   -DENABLE_OMEGA_H=ON \
